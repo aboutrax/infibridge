31e18af70f708215f2a4f0869c7850081732927b
Aboubacar TRAORE <aboubacar.traore@abnov.com>
2026-05-24 01:57:47 +0000

    adding better auth admin plugin

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -4,6 +4,7 @@ import { sveltekitCookies } from 'better-auth/svelte-kit';
 import { env } from '$env/dynamic/private';
 import { getRequestEvent } from '$app/server';
 import { db } from '$lib/server/db';
+import { admin } from 'better-auth/plugins';
 
 export const auth = betterAuth({
 	baseURL: env.ORIGIN,
@@ -11,6 +12,7 @@ export const auth = betterAuth({
 	database: drizzleAdapter(db, { provider: 'pg' }),
 	emailAndPassword: { enabled: true },
 	plugins: [
+		admin(),
 		sveltekitCookies(getRequestEvent) // make sure this is the last plugin in the array
 	]
 });
