1575b2a6eae1418586b6ba440fd9c7777122db23
Aboubacar TRAORE <aboubacar.traore@abnov.com>
2026-05-24 08:23:03 +0000

    add adminPlugin to auth

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -4,15 +4,39 @@ import { sveltekitCookies } from 'better-auth/svelte-kit';
 import { env } from '$env/dynamic/private';
 import { getRequestEvent } from '$app/server';
 import { db } from '$lib/server/db';
-import { admin } from 'better-auth/plugins';
+import { admin as adminPlugin } from 'better-auth/plugins';
+import { ac, admin, user } from '$lib/permissions';
+import { checkAdminExists } from './setup';
 
 export const auth = betterAuth({
 	baseURL: env.ORIGIN,
 	secret: env.BETTER_AUTH_SECRET,
 	database: drizzleAdapter(db, { provider: 'pg' }),
 	emailAndPassword: { enabled: true },
+	databaseHooks: {
+		user: {
+			create: {
+				async before(user) {
+					const exists = await checkAdminExists();
+
+					return {
+						data: {
+							...user,
+							role: !exists ? 'admin' : user.role || 'user'
+						}
+					};
+				}
+			}
+		},
+	},
 	plugins: [
-		admin(),
+		adminPlugin({
+			ac,
+			roles: {
+				admin,
+				user
+			}
+		}),
 		sveltekitCookies(getRequestEvent) // make sure this is the last plugin in the array
 	]
 });
